bd28143c95f084e9105cedd26b972b0e39810734
Vlad Graponov <graponov2012@gmail.com>
2025-04-11 13:35:05 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сохранение времени начала
+
+            DateTime begin_t = DateTime.Now;
+
+            Console.WriteLine("Программа работает");
+
+
+            // Здесь можно добавить код, который вы хотите измерить
+
+
+
+            // Сохранение времени окончания
+            // Сохранение времени окончания
+
+
+            DateTime end_t = DateTime.Now;
+
+            TimeSpan elapsed = end_t - begin_t;
+
+
+            Console.WriteLine("Код выполнился за " + elapsed.TotalSeconds + " сек.");
+        } 
+
+
+    }
+}
